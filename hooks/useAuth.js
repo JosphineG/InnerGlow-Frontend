b32bb6895607056dddcfc4b946ee16dd24db26ad
JosphineG <angeljosphine27@gmail.com>
2024-03-14 18:10:10 +0300

    Fix : localstorage issues

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -2,18 +2,24 @@
 
 const useAuthToken = () => {
   const getItem = () => {
-    const token = localStorage.getItem("innerAuth");
-
-    return { token };
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem("innerAuth");
+      return { token };
+    }
+    return { token: null }; // Return null if localStorage is not available
   };
 
   const clearAuthToken = () => {
-    // Remove the token from local storage
-    localStorage.removeItem("innerAuth");
+    if (typeof window !== 'undefined') {
+      // Remove the token from local storage
+      localStorage.removeItem("innerAuth");
+    }
   };
 
   // Return the token and functions to update and clear it
   return { clearAuthToken, getItem };
 };
+
+
 
 export default useAuthToken;
