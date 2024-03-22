ab1fe5cc2f9d7aa59a006fafb12a9f6c38a8cd70
David-mwas <dmwas704@gmail.com>
2024-03-22 19:02:17 +0300

    FIX : merge authjs
    Co-authored-by: JosphineG <angeljosphine27@gmail.com>

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -6,7 +6,7 @@ const useAuthToken = () => {
       const chatid = localStorage?.getItem("chatId");
       return { token, chatid }; // Return an object with token and chatid
     } else {
-      // return { token: null, chatid: null };
+      return { token: null, chatid: null };
     }
     // return { token: null }; // Return null if localStorage is not available
   };
