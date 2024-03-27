2f83b05e6ffb52068ae63bf55cb74cafef97d1b1
David-mwas <dmwas704@gmail.com>
2024-03-27 21:15:42 +0300

    Update : articles, chatpage
    Co-authored-by: JosphineG <angeljosphine27@gmail.com>

--- src/app/components/ChatNav.jsx
+++ src/app/components/ChatNav.jsx
@@ -8,17 +8,17 @@ function ChatNav({ name }) {
     window.location.href = "/chatlogin";
   };
   return (
-    <header className="w-screen flex justify-between items-center gap-2 px-4 md:px-20 py-4 fixed z-[999] h-[80px] shadow-lg bg-[#e9f1ff]">
+    <header className="w-screen flex justify-between items-center gap-2 px-4 md:px-20 py-4 fixed z-[999] h-[80px] shadow-lg bg-blue-500">
       <h1>InnerGlow</h1>
-      <nav className="flex justify-between items-center gap-4 md:gap-20 capitalize">
+      <nav className="flex justify-between items-center gap-4 md:gap-20 capitalize text-white">
         <a href="/community/articles">articles</a>
         <a href="/history">history</a>
         <h3 className="text-lg p-2 rounded-lg px-4">
-          Hi, <span className="text-blue-500 font-semibold">{name}</span>
+          Hi, <span className="text-white font-bold">{name}</span>
         </h3>
         <button
           onClick={handleLogout}
-          className="bg-[#6495ED47] p-2 rounded-lg px-4"
+          className="bg-white p-2 rounded-lg px-4"
         >
           <span className="text-blue-500 font-semibold">Logout</span>
         </button>
